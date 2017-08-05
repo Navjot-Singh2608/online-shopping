a5d7b665c32af2167b4cfb235aa00edf5983f853
Navjot Singh <navjot2608@gmail.com>
2017-08-05 14:49:36 +0530

    Completed the interface theme and other issue!

--- onlineshopping/src/main/webapp/assets/js/myapp.js
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -8,12 +8,8 @@ $(function(){
 	case 'Contact Us':
 		$('#contact').addClass('active');
 		break;
-	case 'All Products':
-		$('#listProducts').addClass('active');
-		break;
 	default:
-		$('#listProducts').addClass('active');
-	    $('#a_'+menu).addClass('active');
+		$('#home').addClass('active');
 		break;
 	
 	}
